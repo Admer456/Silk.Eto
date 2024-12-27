d5b5f5342dce081c37402e883720c9bdd775c22f
Admer <49600504+Admer456@users.noreply.github.com>
2024-12-27 23:57:11 +0100

    Fix SPDX position

--- src/Silk.Eto/SilkSurface.IWindow.cs
+++ src/Silk.Eto/SilkSurface.IWindow.cs
@@ -1,12 +1,13 @@
-﻿using Eto.Drawing;
+﻿// SPDX-FileCopyrightText: 2024-present Silk.Eto contributors
+// SPDX-License-Identifier: MIT
+
 using Eto.Forms;
 using Silk.NET.Core;
 using Silk.NET.Core.Contexts;
 using Silk.NET.Maths;
 using Silk.NET.Windowing;
 using System.Diagnostics;
-// SPDX-FileCopyrightText: 2024-present Silk.Eto contributors
-// SPDX-License-Identifier: MIT
+
 
 namespace Silk.Eto
 {
